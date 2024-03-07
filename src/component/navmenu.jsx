9566e8c7c4416ca6e09f9606e475b7bbf1911b98
o-gdev <omiwoleoluwagbogo@gmail.com>
2024-03-07 23:59:57 +0100

    first commit

--- src/component/navmenu.jsx
+++ src/component/navmenu.jsx
@@ -5,12 +5,12 @@ const Navmenu = () => {
   return (
     <><div>
       <nav>
-        <div className='flex px-10 py-2 pt-5' style={{ justifyContent: 'space-between' }}>
+        <div className='flex px-10 py-2 pt-2' style={{ justifyContent: 'space-between' }}>
           <div className='flex flex-row'>
             <img src='/assets/logo.png' style={{ width: '40px', height: '40px' }} />
             <h3 className="font-primary font-semibold flex flex-row self-center justify-center mx-3 ">INFINITY MARKET PLACE</h3>
           </div>
-          <ul className="inline">
+          <ul className="inline m-2">
             <li className="inline px-2 font-primary font-semibold"><Link to='/'>Home</Link> </li>
             <li className="inline px-2 font-primary font-semibold"> <Link to='/services'>Services</Link> </li>
             <li className="inline px-2 font-primary font-semibold">About</li>
