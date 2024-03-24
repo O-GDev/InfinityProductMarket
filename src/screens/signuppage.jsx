7aecc1e9299ca94e229cd4c76e56c22509f0fc7b
o-gdev <omiwoleoluwagbogo@gmail.com>
2024-03-25 00:48:55 +0100

    responsive

--- src/screens/signuppage.jsx
+++ src/screens/signuppage.jsx
@@ -6,10 +6,10 @@ import Sidecarousel from '../lilcomponents/sidecarousel';
 function Signuppage() {
   return (
     <div className='fixed inset-0 h-full bg-black bg-opacity-30 backdrop-blur-sm shadow-2xl' style={{}}>
-      <div className='h-full sm:py-10 p-5 pt-3 md:pt-0 md:p-20 ' style={{}}>
+      <div className='h-full sm:py-10 p-5  md:p-20 ' style={{}}>
         <div className='md:columns-2 bg-white h-full '>
             <div className='flex justify-center self-center h-full '>
-            <div className='w-full h-full py-2 md:py-5'>
+            <div className='w-full h-full py-2 pt-2 md:py-5'>
                 <h3 className='font-md font-bold flex justify-center self-center '>Be A Seller!</h3>
                 <div className='flex justify-center self-center'>
                 <div className='' style={{height:0.5,width:80,backgroundColor:'#702EB2'}}></div>
