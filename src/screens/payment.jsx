33467ee06f7dd98c3a462278dbe2bb517d3192e1
O-GDev <omiwoleoluwagbogo@gmail.com>
2024-05-29 05:16:42 -0700

    responsiveness

--- src/screens/payment.jsx
+++ src/screens/payment.jsx
@@ -12,20 +12,20 @@ export const Payment = (props) => {
     <div className='fixed inset-0 h-full bg-black bg-opacity-30 backdrop-blur-sm shadow-2xl ' style={{}}>
     <div className='h-full sm:py-10 p-5  md:p-10 ' style={{}}>
       <div className='md:columns-2 h-full rounded-2xl' style={{backgroundColor:'#F2F2F2'}}>
-      <div className='flex flex-col justify-center h-full'>
+      <div className='flex flex-col justify-center  h-full'>
                     <div className='pl-10' onClick={() => [props.handleClick("subscribe")]} >
          <Icon icon="lets-icons:arrow-left-long-light" width='2rem' height='2rem' />
          </div>
-      <div className='md:columns-3 gap-3 p-12 pt-28 justify-between w-full self-center'>
-              <img src='/assets/Frame 427319632.png' className='flex self-center'  width='60%' height='60%' alt='' />
-              <img src='/assets/Frame 427319633.png' className='flex self-center'  width='60%' height='60%' alt='' />
-              <img src='/assets/Frame 427319634.png' className='flex self-center'  width='60%' height='60%' alt='' />
+      <div className='columns-3 gap-3 p-12 pt-28 justify-between w-full self-center'>
+              <img src='/assets/Frame 427319632.png' className='flex self-center'  width='30%' height='30%' alt='' />
+              <img src='/assets/Frame 427319633.png' className='flex self-center'  width='30%' height='30%' alt='' />
+              <img src='/assets/Frame 427319634.png' className='flex self-center'  width='30%' height='30%' alt='' />
       </div>
 
-      <div className='md:columns-3 gap-3 p-12'>
-              <img src='/assets/Frame 427319635.png' className='flex self-center'  width='60%' height='60%' alt='' />
-              <img src='/assets/Frame 427319636.png' className='flex self-center'  width='60%' height='60%' alt='' />
-              <img src='/assets/Frame 427319637.png' className='flex self-center'  width='60%' height='60%' alt='' />
+      <div className='columns-3 gap-3 p-12'>
+              <img src='/assets/Frame 427319635.png' className='flex self-center'  width='30%' height='30%' alt='' />
+              <img src='/assets/Frame 427319636.png' className='flex self-center'  width='30%' height='30%' alt='' />
+              <img src='/assets/Frame 427319637.png' className='flex self-center'  width='30%' height='30%' alt='' />
      </div>
 
       {/* <div className='p-12'>
