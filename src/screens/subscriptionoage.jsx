fecc49a6b0f1d911f24e20f836329a43ebdb42c9
O-GDev <omiwoleoluwagbogo@gmail.com>
2024-05-29 16:43:11 -0700

    responsiveness

--- src/screens/subscriptionoage.jsx
+++ src/screens/subscriptionoage.jsx
@@ -7,8 +7,8 @@ import { CustomScroll } from 'react-custom-scroll';
 
 export default function Subscriptionoage(props) {
   return (
-    <div className='fixed inset-0 h-full bg-black bg-opacity-30 backdrop-blur-sm shadow-2xl flex justify-center align-middle self-center ' style={{}}>
-    <div className='h-full p-10 md:p-5 ' style={{}}>
+    <div className='fixed inset-0 h-full bg-black bg-opacity-30 backdrop-blur-sm md:shadow-lg flex justify-center align-middle self-center ' style={{}}>
+    <div className='h-full p-5 md:p-5 ' style={{}}>
       <div className='md:columns-2 flex md:flex-row flex-col gap-0 md:h-fit h-full rounded-2xl overflow-auto md:overflow-hidden' style={{backgroundColor:'#F2F2F2'}}>
          {/* <CustomScroll heightRelativeToParent="calc(100%)" > */}
          <div>
@@ -27,20 +27,20 @@ export default function Subscriptionoage(props) {
          <Icon icon="lets-icons:arrow-left-long-light" className='md:w-[2rem] md:h-[2rem]' />
          </div>
               </div>
-              <div className='w-[78%] rounded-t-xl fixed flex md:hidden z-10' style={{backgroundColor:'#F2F2F2'}} >
+              <div className='w-[86%] rounded-t-xl fixed flex md:hidden z-10' style={{backgroundColor:'#F2F2F2'}} >
               <div className='md:pl-10 pl-5 py-5 cursor-pointer ' onClick={() => [props.handleClick("signup")]}>
          <Icon icon="lets-icons:arrow-left-long-light" className='md:w-[2rem] md:h-[2rem]' />
          </div>
               </div>
             {/* </div> */}
-         <div className="pt-10 md:pt-0 flex flex-col justify-center w-full">
+         <div className="pt-12 md:pt-0 flex flex-col justify-center w-full">
          <h3 className='px-10 pt-1 text-xl font-semibold'>Choose a plan</h3> 
           <p className='px-6' style={{fontSize:'12px'}}>choosing a plan for your business will help set your <br />
 business on the right part</p>
           <h3 className='text-center font-bold pt-5'>SUBSCRIPTIONS</h3>
-          <div className='md:columns-3 md:flex flex-row w-full gap-1 px-5 pt-2  ' style={{height:''}}>
+          <div className='md:columns-3 md:flex flex-row w-full gap-2 px-5 pt-2  ' style={{height:''}}>
                 <Link to='' onClick={() => [props.handleClick("payment")]} className='w-full'>
-                  <div id="zoom" className='py-2 ml-2 rounded-3xl shadow-2xl min-w-fit' style={{ backgroundColor: '#B4B4B4', }}>
+                  <div id="zoom" className='py-2 ml-2 rounded-3xl md:shadow-lg min-w-fit my-2 md:my-0' style={{ backgroundColor: '#B4B4B4', }}>
                     <h5 className='text-center p-2'>No Plan</h5>
                     <h4 className='text-center pt-1 opacity-50' ><s>PAY</s></h4>
                     <h6 className='text-center pt-2' style={{ fontSize: '50%' }}>FOR START UPS AND SMALL SCALE BUSINESSES </h6>
@@ -62,7 +62,7 @@ business on the right part</p>
                 </Link>
 
                 <Link to='' onClick={() => props.handleClick('payment')} className='w-full'>
-                  <div id='zoom' className='py-2 ml-2 rounded-3xl shadow-2xl' style={{ backgroundColor: '#DEDEDE', }}>
+                  <div id='zoom' className='py-2 ml-2 rounded-3xl md:shadow-lg min-w-fit my-2 md:my-0' style={{ backgroundColor: '#DEDEDE', }}>
                     <h5 className='text-center p-2'>A Plan</h5>
                     <h4 className='text-center z-10' style={{ lineHeight: 1 }} ><s className='opacity-50'>12 000</s><br /><h4 className='flex justify-center self-center '> 6500<h6 className='flex self-center ' style={{ fontSize: '60%', paddingLeft: 0.5 }}>Monthly</h6></h4></h4>
                     <h6 className='text-center pt-2' style={{ fontSize: '50%' }}>MEDIUM SCALE BUSINESSES </h6>
@@ -87,7 +87,7 @@ business on the right part</p>
 
 
                 <Link to='' onClick={() => props.handleClick('payment')} className='w-full'>
-                  <div id='zoom' className='py-2 ml-2 rounded-3xl shadow-2xl' style={{ backgroundColor: '#FFFEFC', }}>
+                  <div id='zoom' className='py-2 ml-2 rounded-3xl md:shadow-lg' style={{ backgroundColor: '#FFFEFC', }}>
                     <h5 className='text-center p-2 z-10'>Business Plan</h5>
                     <h4 className='text-center ' style={{ lineHeight: 1 }} ><s className='opacity-50'>30 000</s><br /><h4 className='flex justify-center self-center '> 12,000<h6 className='flex self-center ' style={{ fontSize: '60%', paddingLeft: 0.5 }}>Monthly</h6></h4></h4>
                     <h6 className='text-center pt-2' style={{ fontSize: '50%' }}>LARGE SCALE BUSINESS PLANS </h6>
