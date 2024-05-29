6b8b167bb50c055287682ea6b1fac9103400181f
O-GDev <omiwoleoluwagbogo@gmail.com>
2024-05-29 04:36:11 -0700

    responsiveness

--- src/screens/productplacehome.jsx
+++ src/screens/productplacehome.jsx
@@ -65,48 +65,8 @@ useEffect(()=>{
   }
   return (
     <>
-    <div className='flex flex-col w-full max-h-full' style={{paddingBottom:20,}}>
-    <nav className='' style={{position:'fixed',backgroundColor:'#F2F2F2',width:'100%'}}>
-      <div className='flex px-10 py-3' style={{ justifyContent: 'space-between' }}>
-        <div className='flex flex-row'>
-          <img src='/assets/2 2 (1).png' style={{ width: '60px', height: '60px' }} />
-          <div className='bg-white flex flex-row justify-center self-center'>
-          <img src='/assets/Search.png' className='w-4 h-4 my-3 ml-5 font-bold'  />
-          <input placeholder='Search for brands or categories' className='text-sm outline-none pl-1 w-52 font-sembold' /></div>
-          </div>
-        <ul className="inline justify-center self-center">
-          <li className="inline px-5 font-primary font-semibold"><Link to='/'>Home</Link> </li>
-          <li className="inline px-5 font-primary font-semibold"> <Link to='/services'>Today's Deal</Link> </li>
-          <li className="inline px-5 font-primary font-semibold">Discovery</li>
-          <li className="inline px-5 font-primary font-semibold">Blog</li>
-          <li className="inline px-5 font-primary font-semibold">Marketing</li>
-          <li className="inline px-5 font-primary font-semibold">Be a seller</li>
-          <li className="inline px-5 font-primary font-semibold p-3 rounded text-white" style={{ backgroundColor: '#8248BB' }}>Login/Signup</li>
-
-        </ul>
-      </div>
-      <div className='absolute w-full text-white'>
-    {/* <Navmenu />   */}
-      <div className='' style={{fillOpacity:1,opacity:1}}>
-      <div className='flex flex-row justify-center opacity-65 p-3 mx-36 shadow-outline' style={{backgroundColor:'#F2F2F2'}}>
-      <h3 className='text-black font-semibold px-2'>Top Tags:</h3>
-      <h3 className='text-black font-semibold px-2'>Gadgets</h3>
-      <h3 className='text-black font-semibold px-2'>Clothing/Wears</h3>
-      <h3 className='text-black font-semibold px-2'>Appliances</h3>
-      <h3 className='text-black font-semibold px-2'>Cars</h3>
-      <h3 className='text-black font-semibold px-2'>Electronics</h3>
-      <h3 className='text-black font-semibold px-2'>Face Products</h3>
-      <h3 className='text-black font-semibold px-2'>Funiture</h3>
-      <h3 className='text-black font-semibold px-2'>Automobile</h3>
-      <h3 className='text-black font-semibold px-2'>Cosmetics</h3>
-      <h3 className='text-black font-semibold px-2'>Others:</h3>
-
-    </div>
-      </div>
-    
-    </div>  
-      {/* <hr style={{height:0.0001}} ></hr> */}
-    </nav>
+    <div className='absolute flex flex-col w-full full' style={{paddingBottom:20,}}>
+    {/*  */}
 
     <div className='flex pt-20' style={{height:"",}}>
         <div ref={slideRef} className="select-none" style={{}}>
--- src/screens/serviceplacehome.jsx
+++ src/screens/serviceplacehome.jsx
@@ -3,7 +3,7 @@ import { Button, Uline } from '../lilcomponents/style.styles'
 
 export default function Serviceplacehome() {
   return (
-    <div className='absolute flex'>
+    <div className='absolute flex flex-col justify-center align-middle self-center'>
     <h1>hgjm,k.</h1>
     </div>
   )
